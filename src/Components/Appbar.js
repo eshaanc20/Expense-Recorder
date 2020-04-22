26c3b3bc90f274010da2044ac8bada687932dcf4
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-04-22 16:19:10 -0400

    Deployed updated application

--- src/Components/Appbar.js
+++ src/Components/Appbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Components.css';
 import AppBar from '@material-ui/core/AppBar';
+import { Button } from '@material-ui/core';
 
 //the component is the appbar for the application
 class Appbar extends React.Component {
@@ -10,7 +11,11 @@ class Appbar extends React.Component {
             <div id="appbar">
                 <p style={{opacity:'0.5'}}>Eshaan Chaudhari</p>
                 <p> Expense Recorder</p>
-                <p style={{opacity:'0.5'}}>Internship Application</p>
+                <Button 
+                  href="https://github.com/eshaanc20/Expense-Recorder" 
+                  variant="contained"
+                  style={{height: '20%', marginTop: '1%', backgroundColor: '#1a8cff', color: 'white'}}
+                >View on Github</Button>
             </div>
         </AppBar>
       );
